2401ac93e25f30a54632e9aa603d1d75c74f6ca8
fkropfhamer <fabian.kropfhamer@gmail.com>
2022-02-06 21:11:15 +0100

    .

--- faken_gpt2.py
+++ faken_gpt2.py
@@ -1,17 +1,77 @@
 import gpt_2_simple as gpt2
 import os
+import uuid
 
-model_name = "124M"
-if not os.path.isdir(os.path.join("models", model_name)):
-	print(f"Downloading {model_name} model...")
-	gpt2.download_gpt2(model_name=model_name) 
+def main():
+    #train("pheme_non_rumor_simple", "./data/simple_non_rumor.csv")
+    #train("pheme_rumor_simple", "./data/simple_rumor.csv")   
+    #train("pheme_non_rumor_reactions", "./data/reactions_non_rumor.csv")
+    #train("pheme_rumor_reactions", "./data/reactions_rumor.csv") 
 
-file_name = "./data/rumor.csv"
+    generate("pheme_non_rumor_simple", "./data/pheme_simple_generated", 'non_rumor')
+    #generate("pheme_rumor_simple", "./data/pheme_simple_generated", 'rumor')
+    
+def generate(run_name, dire, label):
+    sess = gpt2.start_tf_sess()
 
-sess = gpt2.start_tf_sess()
-gpt2.finetune(sess,
+    load_model(sess, run_name)
+
+    ftexts = []
+
+    
+    max_gen = 4000
+
+    while len(ftexts) < max_gen:
+        print(f"{len(ftexts)} / {max_gen}")
+
+        texts = gpt2.generate(sess, return_as_list=True,  nsamples=10)
+
+        for text in texts:
+            s = text.split("<|startoftext|>")
+            #print(len(s))
+            s.pop()
+            s.pop(0)
+            s = map(lambda x: x.split("<|endoftext|>")[0], s)
+
+            #print(list(s))
+            ftexts += s
+
+    print("saving")
+
+    only_text_dataset_dir = dire
+    for text in ftexts:
+        out_filename = only_text_dataset_dir + f"/{label}/{uuid.uuid4().hex}.txt"
+        os.makedirs(os.path.dirname(out_filename), exist_ok=True)
+        with open(out_filename, "w+") as file:
+            file.write(text)
+
+    #print(texts)
+
+    print("Duplicates ", len([item for item, count in collections.Counter(a).items() if count > 1]))
+
+    #print(len(texts))
+
+def train(run_name, file_name):
+    sess = gpt2.start_tf_sess()
+    finetune(sess, run_name, file_name)
+
+
+def download():
+    model_name = "124M"
+    if not os.path.isdir(os.path.join("models", model_name)):
+	    print(f"Downloading {model_name} model...")
+	    gpt2.download_gpt2(model_name=model_name) 
+
+def load_model(sess, run_name):
+    gpt2.load_gpt2(sess, run_name=run_name)
+
+def finetune(sess, run_name, file_name, steps=1000, model_name="124M"):
+    gpt2.finetune(sess,
               file_name,
               model_name=model_name,
-              steps=1000)   # steps is max number of training steps
+              steps=steps,
+              run_name=run_name
+              )   # steps is max number of training steps
 
-gpt2.generate(sess)
+if __name__ == '__main__':
+    main()
--- pheme_dataset.py
+++ pheme_dataset.py
@@ -4,10 +4,12 @@ from pathlib import Path
 import json
 import numpy as np  
 import csv
+import datetime
 
 def main():
-    #create_dataset()
-    create_csv()
+    create_dataset()
+    create_csv('data/pheme_simple', 'simple')
+    create_csv('data/pheme_with_reactions', 'reactions')
 
 def create_dataset():
     url = "https://figshare.com/ndownloader/files/6453753"
@@ -20,36 +22,87 @@ def create_dataset():
     dataset_dir = os.path.join(os.path.dirname(dataset), 'pheme-rnr-dataset')
 
     only_text_dataset_dir = "./data/pheme_simple"
+    with_reactions_dir = "./data/pheme_with_reactions"
 
     for topic_dir in os.scandir(dataset_dir):
         if topic_dir.is_dir():
             non_rumor_dir = os.path.join(topic_dir, 'non-rumours')
             
             for non_rumor_conversation_dir in os.scandir(non_rumor_dir):
+                source_tweet = None
+                reactions_jsons = []
+
                 source_tweet_dir = os.path.join(non_rumor_conversation_dir, 'source-tweet')
                 for non_rumor_source_tweet_json in Path(source_tweet_dir).glob('*.json'):
                     with open(non_rumor_source_tweet_json, 'r') as file:
                         data = json.load(file)
+                        source_tweet = data
                         out_filename = only_text_dataset_dir + f"/non_rumor/{non_rumor_source_tweet_json.stem}.txt"
                         os.makedirs(os.path.dirname(out_filename), exist_ok=True)
                         with open(out_filename, 'w+') as out_file:
                             out_file.write(data['text'])
 
+                reactions_tweet_dir = os.path.join(non_rumor_conversation_dir, 'reactions')
+                for non_rumor_reactions_json in Path(reactions_tweet_dir).glob('*.json'):
+                    
+                    with open(non_rumor_reactions_json) as file:
+                        data = json.load(file)
+                        created_date = datetime.datetime.strptime(data['created_at'], '%a %b %d %H:%M:%S %z %Y')
+                        data['created_datetime'] = created_date
+
+                        reactions_jsons.append(data)
+                
+
+                sorted_reaction = sorted(reactions_jsons, key=lambda x: x['created_datetime'])
+                conversation_id = os.path.basename(non_rumor_conversation_dir)
+                out_with_reaction_filename = with_reactions_dir + f"/non_rumor/{conversation_id}.txt"
+                os.makedirs(os.path.dirname(out_with_reaction_filename), exist_ok=True)
+                with open(out_with_reaction_filename, 'w+') as file:
+                    texts = [source_tweet['text']]
+                    texts += list(map(lambda x: x['text'], sorted_reaction))
+                    file.write(' ### '.join(texts))
+                
+
             rumor_dir = os.path.join(topic_dir, 'rumours')
 
             for rumor_conversation_dir in os.scandir(rumor_dir):
+                source_tweet = None
+                reactions_jsons = []
+
                 source_tweet_dir = os.path.join(rumor_conversation_dir, 'source-tweet')
                 for rumor_source_tweet_json in Path(source_tweet_dir).glob('*.json'):
                     with open(rumor_source_tweet_json, 'r') as file:
                         data = json.load(file)
+                        source_tweet = data
                         out_filename = only_text_dataset_dir + f"/rumor/{rumor_source_tweet_json.stem}.txt"
                         os.makedirs(os.path.dirname(out_filename), exist_ok=True)
                         with open(out_filename, 'w+') as out_file:
                             out_file.write(data['text'])
 
-def create_csv():
+
+                reactions_tweet_dir = os.path.join(rumor_conversation_dir, 'reactions')
+                for rumor_reactions_json in Path(reactions_tweet_dir).glob('*.json'):
+                    
+                    with open(rumor_reactions_json) as file:
+                        data = json.load(file)
+                        created_date = datetime.datetime.strptime(data['created_at'], '%a %b %d %H:%M:%S %z %Y')
+                        data['created_datetime'] = created_date
+
+                        reactions_jsons.append(data)
+                
+
+                sorted_reaction = sorted(reactions_jsons, key=lambda x: x['created_datetime'])
+                conversation_id = os.path.basename(rumor_conversation_dir)
+                out_with_reaction_filename = with_reactions_dir + f"/rumor/{conversation_id}.txt"
+                os.makedirs(os.path.dirname(out_with_reaction_filename), exist_ok=True)
+                with open(out_with_reaction_filename, 'w+') as file:
+                    texts = [source_tweet['text']]
+                    texts += list(map(lambda x: x['text'], sorted_reaction))
+                    file.write('###'.join(texts))
+
+def create_csv(data_dir, name):
     raw_ds = keras.utils.text_dataset_from_directory(
-        'data/pheme_simple',
+        data_dir,
         batch_size=1
     )
 
@@ -72,7 +125,7 @@ def create_csv():
         classes[class_name] = xs
 
     for class_name, xs in classes.items():
-        with open(f"./data/{class_name}.csv", "w", newline='') as file:
+        with open(f"./data/{name}_{class_name}.csv", "w", newline='') as file:
             csv_writer = csv.writer(file)
             csv_writer.writerow([])
             #file.write(','.join(map(lambda x: x.decode('utf-8'), xs)))
